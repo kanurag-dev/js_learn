5eada2ca0bf600d430754cfb0f2ca7ab5269bbb9
Anurag <88178410+kanurag-dev@users.noreply.github.com>
2026-07-19 17:17:09 +0530

    react and vite setup

---
+++ basicfront/script.js
@@ -0,0 +1,23 @@
+const student={ //object
+    name : "anu",
+    class :12,
+    age:19,
+
+};
+
+
+
+a=(a,b)=>{
+    console.log(a*b);
+}
+
+
+//for-of loop
+/*for(let i of mode){
+    console.log(i);
+}*/
+
+//for in 
+// for(let i in student){
+//     console.log(i,student[i]);
+// }
