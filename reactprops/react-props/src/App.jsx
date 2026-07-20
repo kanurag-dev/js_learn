83cb016e25d043b0c38924095680313c87cc6847
Anurag <88178410+kanurag-dev@users.noreply.github.com>
2026-07-20 14:12:07 +0530

    passing function in props

---
+++ reactprops/react-props/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Car from './components/Car'
+
+function App() {
+
+  return (
+    <div>
+      <Car name="anurag">
+        <h1>I am in app jsx</h1>
+        <p>this is para</p>
+        <p>this is full stack web</p>
+      </Car>
+    </div>
+  )
+}
+
+export default App
